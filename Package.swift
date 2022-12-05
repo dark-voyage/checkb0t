5c14387202a6da42917b51daf1e900cf182d38a9
UwUssimo Robinson <uwussimo@icloud.com>
2022-12-05 18:08:01 +0500

    > s1mple checkb0t <

--- Package.swift
+++ Package.swift
@@ -5,18 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "checkb0t",
+    products: [
+        .executable(name: "checkb0t", targets: ["checkb0t"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "TelegramBotSDK", url: "https://github.com/zmeyc/telegram-bot-swift.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "checkb0t",
-            dependencies: []),
-        .testTarget(
-            name: "checkb0tTests",
-            dependencies: ["checkb0t"]),
+            dependencies: ["TelegramBotSDK"]),
     ]
 )
---
+++ Sources/checkb0t/main.swift
@@ -0,0 +1,39 @@
+import Foundation
+import TelegramBotSDK
+
+let token = readToken(from: "TOKEN")
+
+let bot = TelegramBot(token: token)
+
+let router = Router(bot: bot)
+
+router["help"] = { context in
+    guard let from = context.message?.from else { return false }
+
+    let helpText = "Usage: /greet"
+    context.respondPrivatelyAsync(helpText,
+            groupText: "\(from.firstName), please find usage instructions in a personal message.")
+    return true
+}
+
+router["greet"] = { context in
+    guard let from = context.message?.from else { return false }
+    context.respondAsync("Hello, \(from.firstName)!")
+    return true
+}
+
+router[.newChatMembers] = { context in
+    guard let users = context.message?.newChatMembers else { return false }
+    for user in users {
+        guard user.id != bot.user.id else { return false }
+        context.respondAsync("W3lc0m3, \(user.firstName)!")
+    }
+    return true
+}
+
+print("Ready to accept commands")
+while let update = bot.nextUpdateSync() {
+    try router.process(update: update)
+}
+
+fatalError("Server stopped due to error: \(bot.lastError.unwrapOptional)")
